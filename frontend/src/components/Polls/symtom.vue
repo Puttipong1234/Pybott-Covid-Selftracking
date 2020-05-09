03e65a74cb8763c3b6e63e337fc7214231e26d97
ENWUFT <enwuft@gmail.com>
2020-05-09 21:21:37 +0700

    Add: prettierrc

--- frontend/src/components/Polls/symtom.vue
+++ frontend/src/components/Polls/symtom.vue
@@ -1,22 +1,22 @@
 <template>
   <CRow>
-      <CCol sm="4" lg="2">
-        <CWidgetSimple header="title" text="1,123">
-          <CChartLineSimple style="height:40px" border-color="danger"/>
-        </CWidgetSimple>
-      </CCol>
-    </CRow>
+    <CCol sm="4" lg="2">
+      <CWidgetSimple header="title" text="1,123">
+        <CChartLineSimple style="height:40px" border-color="danger" />
+      </CWidgetSimple>
+    </CCol>
+  </CRow>
 </template>
 
 <script>
-import { CChartLineSimple } from '../Charts/CChartLineSimple'
-import { CChartBarSimple } from '../Charts/CChartBarSimple'
+import { CChartLineSimple } from "../Charts/CChartLineSimple";
+import { CChartBarSimple } from "../Charts/CChartBarSimple";
 export default {
-  name: 'symtonCard',
-  components: { CChartLineSimple , CChartBarSimple }
-}
+  name: "symtonCard",
+  // eslint-disable-next-line vue/no-unused-components
+  components: { CChartLineSimple, CChartBarSimple }
+};
 </script>
 
 <style>
-
 </style>
--- frontend/src/views/dashboard.vue
+++ frontend/src/views/dashboard.vue
@@ -1,96 +1,90 @@
 <template>
-<div ref="Container">
-    <User :userid="this.UID" :pic="this.PIC" :name="this.NAME" v-if="isLoaded"/>
+  <div ref="Container">
+    <User :userid="this.UID" :pic="this.PIC" :name="this.NAME" v-if="isLoaded" />
     <CCardGroup columns class="card-columns">
-      <RadarChart :userid="this.UID" v-if="isLoaded"/>
-      <LineChart :userid="this.UID" v-if="isLoaded"/>
+      <RadarChart :userid="this.UID" v-if="isLoaded" />
+      <LineChart :userid="this.UID" v-if="isLoaded" />
     </CCardGroup>
-</div>
+  </div>
 </template>
 
 <script>
-    
-import RadarChart from "../components/Charts/Radar"
-import LineChart from "../components/Charts/Line"
-import User from "../components/User"
+import RadarChart from "../components/Charts/Radar";
+import LineChart from "../components/Charts/Line";
+import User from "../components/User";
 export default {
   components: {
-        User: User,
-        RadarChart:RadarChart,
-        LineChart:LineChart
-    },
+    User: User,
+    RadarChart: RadarChart,
+    LineChart: LineChart
+  },
   data() {
-      return {
-          loading: true,
-          UID: null,
-          NAME: null,
-          PIC: null,
-          isLogin: false,
-          isClient: false,
-      }
+    return {
+      loading: true,
+      UID: null,
+      NAME: null,
+      PIC: null,
+      isLogin: false,
+      isClient: false
+    };
   },
 
   computed: {
     isLoaded: function() {
-                if (this.hasOwnProperty('UID')
-                    && this.UID != null) {
-                    return true;
-                }
-                return false;
+      // eslint-disable-next-line no-prototype-builtins
+      if (this.hasOwnProperty("UID") && this.UID != null) {
+        return true;
+      }
+      return false;
     }
   },
 
   methods: {
-    initLiff: function(){
-
+    initLiff: function() {
       let loader = this.$loading.show({
-                  // Optional parameters
-                  container: this.fullPage ? null : this.$refs.Container,
-                  canCancel: false,
+        // Optional parameters
+        container: this.fullPage ? null : this.$refs.Container,
+        canCancel: false
       });
 
       window.liff.init(
         {
           liffId: "1654070318-rlnJ7Rpn"
         },
+        // eslint-disable-next-line no-unused-vars
         data => {
           if (window.liff.isLoggedIn()) {
-              window.liff.getProfile().then(value => {
-                console.log(value)
+            window.liff.getProfile().then(value => {
+              console.log(value);
               this.UID = value.userId;
               this.NAME = value.displayName;
               this.PIC = value.pictureUrl;
-          });
+            });
+          } else {
+            window.liff.login();
+            window.liff.getProfile().then(value => {
+              console.log(value);
+              this.UID = value.userId;
+              this.NAME = value.displayName;
+              this.PIC = value.pictureUrl;
+            });
           }
-          else {
-              window.liff.login();
-              window.liff.getProfile().then(value => {
-                console.log(value)
-              this.UID = value.userId;
-              this.NAME = value.displayName;
-              this.PIC = value.pictureUrl;
-          });
-        }
-        setTimeout(() => {loader.hide()},1500)
+          setTimeout(() => {
+            loader.hide();
+          }, 1500);
         },
         err => {
-          console.log('LIFF initialization failed', err)
+          console.log("LIFF initialization failed", err);
         }
-      )
+      );
     }
   },
 
-  created () {
-    
-    this.initLiff()
-
-}
-}
-
-
-
+  created() {
+    this.initLiff();
+  }
+};
 </script>
 
 <style>
-
 </style>
